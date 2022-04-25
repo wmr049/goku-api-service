581c2b8f159e701e3d2b7f40607c033d93f0b0c0
Milton Reis <Milton.Oliveira-ext@ab-Inbev.com>
2022-04-25 16:26:02 -0300

    criado estrutura inicial

---
+++ src/main/java/com/goku/apiservice/GokuApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.goku.apiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GokuApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GokuApiServiceApplication.class, args);
+	}
+
+}
